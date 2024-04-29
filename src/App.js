d6ffaa9902119930c513183b66b435c77c9fe28c
Kshitij Ingole <kshitijingole@kpmg.com>
2024-04-29 12:08:46 +0530

    useRef Hook -Handling forms in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const textTitle = useRef();
+  const hexColor = useRef();
+
+  console.log(textTitle);
+  console.log(hexColor);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = textTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+  };
+
   return (
     <div className="App">
-      
+      <form onSubmit={submit}>
+        <input type="text" ref={textTitle} placeholder="Colour Text .."></input>
+        <input type="color" ref={hexColor}></input>
+        <button>Add color</button>
+      </form>
     </div>
   );
 }
