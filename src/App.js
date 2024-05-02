15e057e1900bd2ac700ab09660d0553a16d6e182
Kshitij Ingole <kshitijingole@kpmg.com>
2024-05-02 15:01:59 +0530

    React Routers - V6

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
 
-const himalayan_peaks = [
-  {name : 'Everest', height: 101256},
-  {name : 'Kanchenjunga', height: 23456},
-  {name : 'K2', height: 2344456}
-]
-
-function List( {data, renderItem, renderEmpty}) {
-  return !data.length ? renderEmpty : (
-    <ul>
-      {data.map((item) => 
-      <li key={item.name}>{renderItem(item)}</li>
-    )}
-    </ul>
-  )
+function Home() {
+  return(
+  <h3>This is Home Page</h3>
+  );
 }
 
+export function Aboutus() {
+  return(
+    <h3>This is About Us Page</h3>
+    );
+}
 
-
-
+export function Contactus() {
+  return(
+    <h3>This is Contact Us Page</h3>
+    );
+}
 
 function App() {
   return(
-    <List 
-    data={himalayan_peaks}
-    renderItem={item => <>{item.name} - {item.height}</>}
-    renderEmpty={<p>Empty</p>}/>
+    <Home />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Aboutus } from './App';
+import { Contactus } from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />}/>
+            <Route path='/about-us' element={<Aboutus />}/>
+            <Route path='/contact-us' element={<Contactus />}/>
+        </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
