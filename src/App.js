acfe0a847ee4ddb8c2f780d559b3fbe31b08f919
Kshitij Ingole <kshitijingole@kpmg.com>
2024-05-02 11:16:18 +0530

    Loading data in a component from the API response.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function GithubDataApi({name, location}) {
+  return (<div>
+    <h3>{name}</h3>
+    <h4>{location}</h4>
+  </div>
+  );
+}
+
 function App() {
   const[data, setData] = useState(null);
   useEffect(() => {
     fetch(
       `https://api.github.com/users/moonhighway`
-    ).then((response) => response.json())
+    )
+    .then((response) => response.json())
     .then(setData);
   }, []);
-  if(data) return(<pre>{JSON.stringify(data, null, 2)}</pre>);
+  if(data) 
+    return (
+    <GithubDataApi 
+    name={data.name} 
+    location={data.location} />
+  );
 
   return (
     <div className="App">
