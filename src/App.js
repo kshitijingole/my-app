6bf8d9f981a3471212f70fe8105d045f87db9958
Kshitij Ingole <kshitijingole@kpmg.com>
2024-05-02 11:30:09 +0530

    Handling the Loading and error state of API response.

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ function GithubDataApi({name, location}) {
 
 function App() {
   const[data, setData] = useState(null);
+  const[loading, setLoading] = useState(false);
+  const[error, setError] = useState(null);
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.github.com/users/moonhighway`
     )
     .then((response) => response.json())
-    .then(setData);
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
   }, []);
-  if(data) 
+
+  if (loading) return(<h3>Loading ...</h3>);
+  if (error) return(<pre>{JSON.stringify(error)}</pre>);
+  if(!data) return null;
     return (
     <GithubDataApi 
     name={data.name} 
     location={data.location} />
-  );
-
-  return (
-    <div className="App">
-      <h1>Displaying Data</h1>
-    </div>
-  );
+    )
 }
 
 export default App;
